077111b3b4bba7bb244072973f7bac8d3c0afd5d
Dan Rednine <r3ddan9@rambler.ru>
2021-10-29 12:20:25 +0300

    Add files via upload

--- functions/presets.php
+++ functions/presets.php
@@ -8,15 +8,7 @@
 
 	$style = "default";
 
-	function isMobile() 
-
-	{
-
-		return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
-
-	};
-
-	if(isMobile())
+	if(preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]))
 
 	{
 
