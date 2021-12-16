fdc1dc8b5909318304147ead115fdf8de700d226
Danil Rednine <r3ddan9@rambler.ru>
2021-12-16 16:45:05 +0300

    Add files via upload

--- scripts/attachments.js
+++ scripts/attachments.js
@@ -4,6 +4,68 @@ function uploadFile(target)
 
 {
 
-    document.getElementById("chooseFileButtonLabel").innerHTML = target.files[0].name;
+	document.getElementById("chooseFileButtonLabel").innerHTML = target.files[0].name;
 
 };
+
+function scrollTopPermanent()
+
+{
+
+	window.scrollTo({top: 0, behavior: "smooth"});
+
+};
+
+function main()
+
+{
+
+	"use strict";
+
+	let heightThreshold = 500;
+
+	let buttonToTop = document.getElementById("buttonToTop");
+
+	let totalHeight, limitHeight, currentHeight;
+
+	function getHeightMetrics()
+
+	{
+
+		totalHeight = 0;
+
+		limitHeight = totalHeight + heightThreshold;
+
+		currentHeight = window.scrollY;
+
+	};
+
+	function onScrollFunctions() 
+
+	{
+
+		getHeightMetrics();
+
+		if (currentHeight > limitHeight) 
+
+		{
+
+			buttonToTop.style.display = "block";
+
+		}
+
+		else
+
+		{
+
+			buttonToTop.style.display = "none";
+
+		};
+
+	};
+
+	document.addEventListener("scroll", onScrollFunctions);
+
+};
+
+main();
