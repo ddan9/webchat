da09c55740074490c3d40e4604f68d637d45d7ad
Danil Rednine <r3ddan9@rambler.ru>
2021-12-29 11:23:17 +0300

    Add files via upload

--- scripts/attachments.js
+++ scripts/attachments.js
@@ -4,27 +4,63 @@ function uploadFile(input)
 
 {
 
+	"use strict";
+
+	function showSelectedFiles()
+
+	{
+
+		if (useSimpleShow == "true")
+
+		{
+
+			if (inputFilesCount > 1)
+
+			{
+
+				inputFileName = "Selected " + inputFilesCount + " files";
+
+			}
+
+			else
+
+			{
+
+				inputFileName = input.files[0].name;
+
+			};
+
+		}
+
+		else
+
+		{
+
+			inputFileName = "";
+
+			for (i = 0; i < inputFilesCount; i++)
+
+			{
+
+				inputFileName += input.files[i].name + " ";
+
+			};
+
+		};
+
+	};
+
+	var useSimpleShow = "true";
+
 	var validationRegexp = /<xmp>|<\/xmp>/gi;
 
 	var validationReplacement = "<rofl>";
 
 	var inputFilesCount = input.files.length;
 
-	if (inputFilesCount > 1)
+	var i, inputFileName;
 
-	{
-
-		var inputFileName = "Selected " + inputFilesCount + " files";
-
-	}
-
-	else
-
-	{
-
-		var inputFileName = input.files[0].name;
-
-	};
+	showSelectedFiles();
 
 	var validatedFileName = inputFileName.replace(validationRegexp, validationReplacement);
 
