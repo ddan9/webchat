e520fa4cfbd5715341c37591705fb06b97d17770
Danil Rednine <r3ddan9@rambler.ru>
2022-06-02 14:43:21 +0300

    Add files via upload

--- functions/get.php
+++ functions/get.php
@@ -1,6 +1,6 @@
 <?php
 
-	function send_messages_database($use_databases_encryption, $recieve_client_password, $client_password_to_send_database, $PASSWORD, $databases_messages_path, $encryption_cipher, $salt_global, $databases_password, $salt_messages, $encryption_options, $encryption_iv)
+	function send_messages_database($use_databases_encryption, $time_messages_format, $custom_time_set, $date_messages_format, $custom_date_set, $recieve_client_password, $client_password_to_send_database, $PASSWORD, $databases_messages_path, $encryption_cipher, $salt_global, $databases_password, $salt_messages, $encryption_options, $encryption_iv)
 
 	{
 
@@ -34,7 +34,23 @@
 
 			{
 
-				$chat_database = null;
+				$time = base64_encode(date($time_messages_format, strtotime($custom_time_set)));
+
+				$date = base64_encode(date($date_messages_format, strtotime($custom_date_set)));
+
+				$device = base64_encode("Server");
+
+				$nickname = base64_encode("System");
+
+				$message = base64_encode("Authentication error!");
+
+				$address = base64_encode("Notify");
+
+				$content = array("time" => $time, "date" => $date, "device" => $device, "nickname" => $nickname, "message" => $message, "address" => $address);
+
+				$message = array("0" => $content);
+
+				$chat_database = base64_encode(json_encode($message, JSON_PRETTY_PRINT));
 
 			};
 
@@ -80,7 +96,7 @@
 
 		case "messages":
 
-			echo send_messages_database($use_databases_encryption, $recieve_client_password, $client_password_to_send_database, $PASSWORD, $databases_messages_path, $encryption_cipher, $salt_global, $databases_password, $salt_messages, $encryption_options, $encryption_iv);
+			echo send_messages_database($use_databases_encryption, $time_messages_format, $custom_time_set, $date_messages_format, $custom_date_set, $recieve_client_password, $client_password_to_send_database, $PASSWORD, $databases_messages_path, $encryption_cipher, $salt_global, $databases_password, $salt_messages, $encryption_options, $encryption_iv);
 
 			break;
 
