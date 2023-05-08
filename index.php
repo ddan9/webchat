8c2067df8d72dc7bf298be41d478bfb24d925ab8
ddan9 <r3ddan9@rambler.ru>
2023-05-08 17:46:54 +0300

    small changes

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-	header("Refresh: 2; url=./lobby/");
-
 	require_once("./functions/presets.php");
 
 	require_once("./templates/loading.html");
+
+	require_once("./templates/index.html");
 
 ?>
